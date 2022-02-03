a3115c7e2c2a88d6ae8540c06c322dbdf8220fd3
Sudipto Chandra <dipu.sudipta@gmail.com>
2022-02-03 17:24:15 +0600

    Next version

--- scripts/push_tag.sh
+++ scripts/push_tag.sh
@@ -2,7 +2,7 @@
 
 VERSION=$(sed -nr "s/[^']+'([^']+)'$/\1/p" pyease_grpc/__init__.py)
 
-git push --delete origin "v$VERSION"
-git tag -d "v$VERSION"
+# git push --delete origin "v$VERSION"
+# git tag -d "v$VERSION"
 git tag "v$VERSION"
 git push --tags
